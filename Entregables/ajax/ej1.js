258f2a65c071cf76d4424c06d1f86de95ca369f3
kiwii333 <raulperalcortes13@gmail.com>
2025-12-01 23:49:34 +0100

    mod

--- Entregables/ajax/ej1.js
+++ Entregables/ajax/ej1.js
@@ -1,21 +1,28 @@
-usuarios = document.getElementById('div');
+const body = document.getElementById('body') 
 
+fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then((data) => {
+        // Mostrar en consola el primer elemento del array `results`
+        console.log(data.results[0]);
+        // Mostrar en consola el campo `name.title` dentro de results[0]
+        console.log(data.results[0].name.title);
 
-fetch('https://fakerapi.it/api/v2/persons?_quantity=1')
+        
+        document.getElementById('body').innerHTML += `
+        <img src="${data.results[0].picture.large}" alt="User Picture"><br>
+        ${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last}<br>
+        Email: ${data.results[0].email} <br>
+        ${data.results[0].location.street.number} ${data.results[0].location.street.name}<br>
+        ${data.results[0].location.city}`;
 
-.then(response => {
-    return response.json()
-})
+        
 
-.then((data) =>{
-    console.log(data.data)
-    let datos = document.createElement('p');
-    datos.texContent = 
-    `${data.results(picture.large)} \n
-    ${data.results(name.title)} ${data.results(first)} ${data.results(last)} \n
-    ${data.results(email)}\n
-    ${data.results(location.number)} ${data.results(location.city)} ${data.results(location.country)}` ;
-    usuarios.appendChild(datos);
+        
+
+        body.appendChild(datos);
+        
     })
-;
+    .catch(err => console.error('Fetch error:', err));
+
 
